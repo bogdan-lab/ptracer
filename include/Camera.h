995523d20b36173626be82c9f8d34895f6d515b9
bogdan <lakatosh@phystech.edu>
2021-04-03 19:03:23 +0300

    now we can see what it is drawing!

--- include/Camera.h
+++ include/Camera.h
@@ -2,32 +2,51 @@
 #define CAMERA_H
 
 #include "GeoVec.h"
+#include "Ray.h"
 
 #include <cstdlib>
 #include <random>
+#include <vector>
 
 class Camera {
 private:
     size_t width_{600};
     size_t height_{400};
-    double dist_to_plane_{1.0};
+    double dist_to_plane_{500};
     std::mt19937 rnd_;
     std::uniform_real_distribution<double> distrib_{0.0, 1.0};
     GeoVec pos_;
 
+    Ray CreateRay(size_t x_coor, size_t y_coor) const {
+        GeoVec dir = {x_coor - pos_.x_, y_coor - pos_.y_, dist_to_plane_};
+        return {pos_, dir};
+    }
+
 public:
     Camera() : rnd_(42), pos_(width_/2, height_/2, -dist_to_plane_) {}
 
-    Camera(size_t seed) : rnd_(seed), pos_(width_/2, height_/2, -dist_to_plane_)  {}
+    explicit Camera(size_t seed) : rnd_(seed), pos_(width_/2, height_/2, -dist_to_plane_)  {}
 
-    GeoVec GetCameraRay() {
+    Ray GetCameraRay() {
         double x_rnd = distrib_(rnd_);
         double y_rnd = distrib_(rnd_);
-        return {x_rnd*width_ - pos_.x_,
-                y_rnd*height_ - pos_.y_,
-                dist_to_plane_};
+        return CreateRay(x_rnd*width_, y_rnd*height_);
     }
 
+    std::vector<Ray> GetCameraRayGrid() const {
+        std::vector<Ray> all_rays;
+        all_rays.reserve(width_*height_);
+        for(size_t row=0; row<height_; row++){
+            for(size_t col=0; col<width_; col++){
+                all_rays.push_back(CreateRay(col, row));
+            }
+        }
+        return all_rays;
+    }
+
+    size_t GetWidth() const {return width_;}
+    size_t GetHeight() const {return height_;}
+    double GetDistToPlane() const {return dist_to_plane_;}
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,32 @@
 #include "Objects.h"
 #include "Camera.h"
 
-
-
+#include <vector>
+#include <fstream>
 
 int main(){
+
+    Camera cam;
+    std::vector<Ray> cam_rays = cam.GetCameraRayGrid();
+
+    Sphere sphere{{100, 100, 100}, 100};
+
+    std::ofstream image("test.pbm");
+    image << "P1\n";
+    image << cam.GetWidth() << ' ' << cam.GetHeight() << '\n';
+    size_t pic_width = cam.GetWidth();
+    for (size_t i=0; i<cam_rays.size(); i++) {
+        if(sphere.GetClosesDist(cam_rays[i])) {
+            image << "1 ";
+        }
+        else {
+            image << "0 ";
+        }
+        if((i+1)/pic_width>0 && (i+1)%pic_width==0) {
+            image << '\n';
+        }
+    }
+
+
     return 0;
 }
