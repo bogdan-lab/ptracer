007d5e123a3b2a917e1c5d8bac52e464dee39aba
bogdan <lakatosh@phystech.edu>
2021-04-03 19:40:32 +0300

    now picture is with color (still ppm...)

--- include/Objects.h
+++ include/Objects.h
@@ -5,18 +5,52 @@
 #include "Ray.h"
 
 #include <optional>
+#include <algorithm>
+#include <iostream>
 
 struct Color {
-    double red_;
-    double green_;
-    double blue_;
+    static const uint8_t MAX_COLOR_VALUE = 255u;
+    uint8_t red_{0};
+    uint8_t green_{0};
+    uint8_t blue_{0};
+
+    Color() = default;
+
+    Color(uint32_t R, uint32_t G, uint32_t B) {
+        red_ = std::clamp(R, 0u, static_cast<uint32_t>(MAX_COLOR_VALUE));
+        green_ = std::clamp(G, 0u, static_cast<uint32_t>(MAX_COLOR_VALUE));
+        blue_ = std::clamp(B, 0u, static_cast<uint32_t>(MAX_COLOR_VALUE));
+    }
+
 };
 
+inline std::ostream& operator<<(std::ostream& out, uint8_t val) {
+    return out << static_cast<uint32_t>(val);
+}
 
-struct Object {
+inline std::ostream& operator << (std::ostream& out, const Color& c) {
+    return out << c.red_ << ' ' << c.green_ << ' ' << c.blue_;
+}
+
+
+class Object {
+private:
     Color color_;
-    double emission_;
-    //int type
+    double emission_{0.0};
+public:
+
+    Object& SetColor(const Color& col) {
+        color_ = col;
+        return *this;
+    }
+
+    Object& SetEmission(double g_emit) {
+        emission_ = g_emit;
+        return *this;
+    }
+
+    const Color& GetColor() const {return color_;}
+    double GetEmission() const {return emission_;}
 
     virtual std::optional<double> GetClosesDist(const Ray& ray) const = 0;
     virtual GeoVec GetNorm(const GeoVec& p) const = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,19 @@ int main(){
     std::vector<Ray> cam_rays = cam.GetCameraRayGrid();
 
     Sphere sphere{{100, 100, 100}, 100};
-
-    std::ofstream image("test.pbm");
-    image << "P1\n";
+    sphere.SetColor({255, 139, 87});
+    std::ofstream image("test.ppm");
+    image << "P3\n";
     image << cam.GetWidth() << ' ' << cam.GetHeight() << '\n';
-    size_t pic_width = cam.GetWidth();
+    image << Color::MAX_COLOR_VALUE << '\n';
     for (size_t i=0; i<cam_rays.size(); i++) {
         if(sphere.GetClosesDist(cam_rays[i])) {
-            image << "1 ";
+            image << sphere.GetColor() << '\n';
         }
         else {
-            image << "0 ";
-        }
-        if((i+1)/pic_width>0 && (i+1)%pic_width==0) {
-            image << '\n';
+            image << Color{} << '\n';
         }
     }
-
 
     return 0;
 }
