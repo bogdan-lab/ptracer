29f89c7e752ec20cf904dc8294351cda2e2db40e
bogdan <lakatosh@phystech.edu>
2021-04-13 21:04:25 +0300

    Added triangle structure

--- include/Objects.h
+++ include/Objects.h
@@ -44,7 +44,7 @@ public:
 
     virtual std::optional<double> GetClosesDist(const Ray& ray) const = 0;
     virtual GeoVec GetNorm(const GeoVec& p) const = 0;
-    virtual void Reflect(Ray& ray_in, double dist) const = 0;
+    virtual void Reflect(Ray& ray, double dist) const = 0;
     virtual ~Object() = default;
 };
 
@@ -79,7 +79,42 @@ public:
     }
 };
 
+class Triangle : public Object {
+private:
+    GeoVec p0_;
+    GeoVec p1_;
+    GeoVec p2_;
+    GeoVec norm_;
+    double D_; 	//coefficient for plain Ax + By + Cz + D = 0
 
+public:
+    Triangle() = delete;
+    Triangle(const GeoVec& gp0, const GeoVec& gp1, const GeoVec& gp2)
+        : p0_(gp0), p1_(gp1), p2_(gp2), norm_(0,0,0) {
+        GeoVec lhs{p0_, p1_};
+        GeoVec rhs{p0_, p2_};
+        norm_ = lhs.Cross(rhs).Norm();
+        D_ = -norm_.Dot(p0_);
+    }
+
+    std::optional<double> GetClosesDist(const Ray &ray) const override;
+
+    GeoVec GetNorm(const GeoVec& /*p*/) const override {return norm_;}
+
+    //TODO code duplication looks like Reflect should be defined in separate class and belong to Object field
+    void Reflect(Ray& ray, double dist) const override {
+        const auto& ray_dir = ray.GetDir();
+        const auto& ray_pos = ray.GetPos();
+        ray.Advance(dist);
+        GeoVec check_vec{p0_, ray_pos};
+        while(check_vec.Dot(norm_)<=0) {
+            ray.StepBack(GetHitPrecision());
+        }
+        GeoVec norm = GetNorm(ray_pos);
+        ray.UpdateDirection(ray_dir - 2*ray_dir.Dot(norm)*norm);
+
+    }
+};
 
 
 
--- src/objects.cpp
+++ src/objects.cpp
@@ -12,3 +12,26 @@ std::optional<double> Sphere::GetClosesDist(const Ray& ray) const {
     if(D_quarter<=0) return std::nullopt; 	//==0 - ignore when ray only touch the surface
     return dist_proj - std::sqrt(D_quarter);
 }
+
+
+
+std::optional<double> Triangle::GetClosesDist(const Ray &ray) const {
+    //find ray crossection with the plain
+    const auto& ray_dir = ray.GetDir();
+    const auto& ray_pos = ray.GetPos();
+    double den = ray_dir.Dot(norm_);
+    if (den<=0) return std::nullopt; 	//ray is directed away!
+    double num = ray_pos.Dot(norm_);
+    double t = -(D_ + num)/den;
+    GeoVec cross_pt = ray_pos + t*ray_dir;
+    //check if point inside triangle
+    GeoVec to_p0{cross_pt, p0_};
+    GeoVec to_p1{cross_pt, p1_};
+    GeoVec to_p2{cross_pt, p2_};
+    GeoVec v0 = to_p0.Cross(to_p1);
+    GeoVec v1 = to_p1.Cross(to_p2);
+    GeoVec v2 = to_p2.Cross(to_p2);
+    if(v0.Dot(norm_)<=0 || v1.Dot(norm_)<=0 || v2.Dot(norm_)) return std::nullopt;
+    //calculate distance
+    return dist_btw_points(ray_pos, cross_pt);
+}
