d59802ceb8a7ee447cf9a6602a66661e3e66f3f8
bogdan-lab <lakatosh@phystech.edu>
2021-05-08 20:25:22 +0300

    constexpr GeoVec

--- include/GeoVec.h
+++ include/GeoVec.h
@@ -9,7 +9,7 @@ struct GeoVec {
   double x_, y_, z_;
 
   GeoVec() = delete;
-  GeoVec(double x, double y, double z) : x_(x), y_(y), z_(z) {}
+  constexpr GeoVec(double x, double y, double z) : x_(x), y_(y), z_(z) {}
   GeoVec(const GeoVec& start, const GeoVec& end) {
     x_ = end.x_ - start.x_;
     y_ = end.y_ - start.y_;
@@ -27,44 +27,48 @@ struct GeoVec {
     return *this;
   }
 
-  double Dot(const GeoVec& other) const {
+  constexpr double Dot(const GeoVec& other) const {
     return other.x_ * x_ + other.y_ * y_ + other.z_ * z_;
   }
 
-  GeoVec Cross(const GeoVec& other) const {
+  constexpr GeoVec Cross(const GeoVec& other) const {
     return {y_ * other.z_ - z_ * other.y_, z_ * other.x_ - x_ * other.z_,
             x_ * other.y_ - y_ * other.x_};
   }
 };
 
-inline GeoVec operator/(const GeoVec& lhs, double rhs) {
+inline constexpr GeoVec operator/(const GeoVec& lhs, double rhs) {
   assert(rhs != 0);
   return {lhs.x_ / rhs, lhs.y_ / rhs, lhs.z_ / rhs};
 }
 
-inline GeoVec operator*(const GeoVec& lhs, double rhs) {
+inline constexpr GeoVec operator*(const GeoVec& lhs, double rhs) {
   return {lhs.x_ * rhs, lhs.y_ * rhs, lhs.z_ * rhs};
 }
 
-inline GeoVec operator*(double lhs, const GeoVec& rhs) { return rhs * lhs; }
+inline constexpr GeoVec operator*(double lhs, const GeoVec& rhs) {
+  return rhs * lhs;
+}
 
-inline GeoVec operator+(const GeoVec& lhs, const GeoVec& rhs) {
+inline constexpr GeoVec operator+(const GeoVec& lhs, const GeoVec& rhs) {
   return {lhs.x_ + rhs.x_, lhs.y_ + rhs.y_, lhs.z_ + rhs.z_};
 }
 
-inline GeoVec operator-(const GeoVec& lhs, const GeoVec& rhs) {
+inline constexpr GeoVec operator-(const GeoVec& lhs, const GeoVec& rhs) {
   return {lhs.x_ - rhs.x_, lhs.y_ - rhs.y_, lhs.z_ - rhs.z_};
 }
 
-inline bool operator==(const GeoVec& lhs, const GeoVec& rhs) {
+inline constexpr bool operator==(const GeoVec& lhs, const GeoVec& rhs) {
   return lhs.x_ == rhs.x_ && lhs.y_ == rhs.y_ && lhs.z_ == rhs.z_;
 }
 
-inline bool operator!=(const GeoVec& lhs, const GeoVec& rhs) {
+inline constexpr bool operator!=(const GeoVec& lhs, const GeoVec& rhs) {
   return lhs.x_ != rhs.x_ || lhs.y_ != rhs.y_ || lhs.z_ == rhs.z_;
 }
 
-inline GeoVec operator-(const GeoVec& v) { return {-v.x_, -v.y_, -v.z_}; }
+inline constexpr GeoVec operator-(const GeoVec& v) {
+  return {-v.x_, -v.y_, -v.z_};
+}
 
 inline double dist_btw_points(const GeoVec& lhs, const GeoVec& rhs) {
   GeoVec tmp{lhs, rhs};
