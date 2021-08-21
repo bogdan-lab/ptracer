0ddfbcb885f3eaae7a1a586380457923f9cdc208
bogdan-lab <lakatosh@phystech.edu>
2021-08-21 23:52:33 +0300

    add matrix class

--- include/Matrix.h
+++ include/Matrix.h
@@ -6,51 +6,34 @@
 #include "GeoVec.h"
 
 class Matrix3x3 {
-  //    TODO CHANGE TO std::valarray ot std::vector! indexes are much better
-  //    here!
-  GeoVec c0_{1, 0, 0};
-  GeoVec c1_{0, 1, 0};
-  GeoVec c2_{0, 0, 1};
-
- public:
-  Matrix3x3() = default;
-  Matrix3x3(GeoVec col0, GeoVec col1, GeoVec col2)
-      : c0_(std::move(col0)), c1_(std::move(col1)), c2_(std::move(col2)) {}
-
-  Matrix3x3& SetCol0(GeoVec c) {
-    c0_ = std::move(c);
-    return *this;
-  }
-
-  Matrix3x3& SetCol1(GeoVec c) {
-    c0_ = std::move(c);
-    return *this;
-  }
-
-  Matrix3x3& SetCol2(GeoVec c) {
-    c0_ = std::move(c);
-    return *this;
-  }
-
-  Matrix3x3 CalcReverse() const {
-    double det = CalcDet();
-    Matrix3x3 trans = GetTranspose();
-    Matrix3x3 conf;
-    // SHOULD BE CALCULATED ON TRANS!!!!!
-    conf.SetCol0({Det2x2(c1_.y_, c2_.y_, c1_.z_, c2_.z_),
-                  Det2x2(c1_.x_, c2_.x_, c1_.z_, c2_.z_),
-                  Det2x2(c1_.x_, c2_.x_, c1_.y_, c2_.y_)})
-        .SetCol1({Det2x2(c0_.y_, c2_.y_, c0_.z_, c2_.z_),
-                  Det2x2(c0_.x_, c2_.x_, c0_.z_, c2_.z_),
-                  Det2x2(c0_.x_, c2_.x_, c0_.y_, c2_.y_)})
-        .SetCol2({Det2x2(c0_.y_, c1_.y_, c0_.z_, c1_.z_),
-                  Det2x2(c0_.x_, c1_.x_, c0_.z_, c1_.z_),
-                  Det2x2(c0_.x_, c1_.x_, c0_.y_, c1_.y_)});
-    return {};
-  }
+  GeoVec c0_;
+  GeoVec c1_;
+  GeoVec c2_;
 
   static double Det2x2(double v00, double v01, double v10, double v11) {
     return v00 * v11 - v10 * v01;
+  }
+
+ public:
+  Matrix3x3(GeoVec p0, GeoVec p1, GeoVec p2) : c0_(p0), c1_(p1), c2_(p2) {}
+
+  Matrix3x3 CalcReverse() const {
+    double det = CalcDet();
+    Matrix3x3 tr = GetTranspose();
+
+    double a00 = Det2x2(tr.c1_.y_, tr.c2_.y_, tr.c1_.z_, tr.c2_.z_) / det;
+    double a01 = -Det2x2(tr.c0_.y_, tr.c2_.y_, tr.c0_.z_, tr.c2_.z_) / det;
+    double a02 = Det2x2(tr.c0_.y_, tr.c1_.y_, tr.c0_.z_, tr.c1_.z_) / det;
+
+    double a10 = -Det2x2(tr.c1_.x_, tr.c2_.x_, tr.c1_.z_, tr.c2_.z_) / det;
+    double a11 = Det2x2(tr.c0_.x_, tr.c2_.x_, tr.c0_.z_, tr.c2_.z_) / det;
+    double a12 = -Det2x2(tr.c0_.x_, tr.c1_.x_, tr.c0_.z_, tr.c1_.z_) / det;
+
+    double a20 = Det2x2(tr.c1_.x_, tr.c2_.x_, tr.c1_.y_, tr.c2_.y_) / det;
+    double a21 = -Det2x2(tr.c0_.x_, tr.c2_.x_, tr.c0_.y_, tr.c2_.y_) / det;
+    double a22 = Det2x2(tr.c0_.x_, tr.c1_.x_, tr.c0_.y_, tr.c1_.y_) / det;
+    return {GeoVec{a00, a10, a20}, GeoVec{a01, a11, a21},
+            GeoVec{a02, a12, a22}};
   }
 
   double CalcDet() const {
@@ -65,8 +48,9 @@ class Matrix3x3 {
   }
 
   GeoVec ApplyToVec(const GeoVec& vec) const {
-    return {vec.Dot({c0_.x_, c1_.x_, c2.x_}), vec.Dot({c0_.y_, c1_.y_, c2.y_}),
-            vec.Dot({c0_.z_, c1_.z_, c2.z_})};
+    return {vec.Dot({c0_.x_, c1_.x_, c2_.x_}),
+            vec.Dot({c0_.y_, c1_.y_, c2_.y_}),
+            vec.Dot({c0_.z_, c1_.z_, c2_.z_})};
   }
 };
 
