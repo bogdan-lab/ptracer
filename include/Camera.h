c766202bfbc511602c520245207f9cafebd04ea2
bogdan-lab <lakatosh@phystech.edu>
2021-09-08 00:15:06 +0300

    Camera documentation

--- include/Camera.h
+++ include/Camera.h
@@ -11,8 +11,22 @@
 #include "Pixel.h"
 #include "Ray.h"
 
+// TODO maybe funny to add possibility to set camera position according to x and
+// y coordinates
+
+// TODO maybe it is batter to gather all these static general settings into some
+// config class
+/**
+ * This class contains general information about camera and picture. This is
+ * coordinates of picture edges, distance from camera to the picture plane. For
+ * now camera position in x and y coordinates is laways in the center of the
+ * picture. Picture coordinates are set in the way that top left corner
+ * coordinates are (0, 0). Class also contains information about pixel number in
+ * the picture. It also defines few general parameters, such as: empty color -
+ * color which is obtained in the case when light beam does not hit any material
+ * samples per pixel - number of rays emited to calculate color in one pixel
+ */
 class Camera {
-  // Top left point is (0,0)
  private:
   // input fields
   static size_t SAMPLES_PER_PIXEL;
@@ -29,54 +43,86 @@ class Camera {
   mutable std::mt19937 rnd_{0};
 
  public:
+  /**
+   * Sets number of rays which will be emitted for each pixel in order to
+   * calculate its color (default 10)
+   */
   static void SetSamplePerPixel(size_t g_num) {
     assert(g_num != 0);
     SAMPLES_PER_PIXEL = g_num;
   }
-
+  /**
+   * Sets background color - the one which will be assigned to pixel for which
+   * all rays miss all objects (default black)
+   */
   static constexpr void SetEmptyColor(Color g_c) {
     EMPTY_COLOR = std::move(g_c);
   }
-
+  /**
+   * By default picture pixel resolution is 600x400
+   * background color is black
+   * for each pixel 10 rays will be used to calculate its color
+   * distance from camera to the picture plane 500
+   * horizontal span of the picture - [0.0, 600.0]
+   * vertical span of the picture - [0.0, 400.0]
+   */
   constexpr Camera() = default;
-
+  /** Set left coordinate edge of the picutre (default 0.0)*/
   constexpr Camera& SetLeftCoor(double g_coor) {
     left_coor_ = g_coor;
     return *this;
   }
+  /** Set right coordinate edge of the picture (default 600.0)*/
   constexpr Camera& SetRightCoor(double g_coor) {
     right_coor_ = g_coor;
     return *this;
   }
+  /** Set top coordinate edge of the picture (default 0.0)*/
   constexpr Camera& SetTopCoor(double g_coor) {
     top_coor_ = g_coor;
     return *this;
   }
+  /** Set bot coordinate edge of the picture (default 400.0)*/
   constexpr Camera& SetBotCoor(double g_coor) {
     bot_coor_ = g_coor;
     return *this;
   }
+  /** Set number of pixels in picture in horizontal direction (default 600)*/
   constexpr Camera& SetWidthInPixel(uint32_t g_num) {
     w_px_num_ = g_num;
     return *this;
   }
+  /** Set number of pixels in picture in vertical direction (default 400)*/
   constexpr Camera& SetHeightInPixel(uint32_t g_num) {
     h_px_num_ = g_num;
     return *this;
   }
+  /** Set distance from camera to the picture plane (default 500)*/
   constexpr Camera& SetDistToPlane(double g_dist) {
     dist_to_plane_ = g_dist;
     return *this;
   }
-
+  /**
+   * For generating rays camera uses random generator. This method allows to
+   * provide seed for this generator
+   */
   Camera& SetSeed(size_t new_s) {
     rnd_.seed(new_s);
     return *this;
   }
-
+  /**
+   * Method returns a collection of rays for the pixel with givem index.
+   *
+   * @param idx - pixel idx on the screen. Assume that pixels are counted from
+   * the top left corner to the right
+   *
+   * @warning method expects pixel index to have valid value and also that all
+   * parameters of picture (coordinates, pixels number) also have valid values
+   *
+   * @return collection of rays for given pixes which needs to be emitted n
+   * order to calculate its color
+   */
   std::vector<Ray> GetPixelRays(size_t idx) const {
-    // idx - pixel idx on the screen. Assume that pixels are counted from the
-    // top left corner to the right
     assert(idx < GetPxNum());
     assert(right_coor_ > left_coor_);
     assert(top_coor_ < bot_coor_);
@@ -98,15 +144,23 @@ class Camera {
     }
     return px_rays;
   }
-
+  /** @return background color (default black)*/
   static constexpr const Color& GetEmptyColor() { return EMPTY_COLOR; }
+  /** @return picture left coordinate edge (default 0.0)*/
   constexpr double GetLeftCoor() const { return left_coor_; }
+  /** @return picture right coordinate edge (default 600.0)*/
   constexpr double GetRightCoor() const { return right_coor_; }
+  /** @return picture top coordinate edge (default 0.0)*/
   constexpr double GetTopCoor() const { return top_coor_; }
+  /** @return picture bottom coordinate edge (default 400.0)*/
   constexpr double GetBotCoor() const { return bot_coor_; }
+  /** @return picture width in pixel (default 600)*/
   constexpr uint32_t GetWidthInPx() const { return w_px_num_; }
+  /** @return picture height in pixel (default 400)*/
   constexpr uint32_t GetHeightInPx() const { return h_px_num_; }
+  /** @return total number of pixels in the picture (default 600*400)*/
   constexpr uint32_t GetPxNum() const { return w_px_num_ * h_px_num_; }
+  /** @return distance from camera to the picture plane (default 500.0)*/
   constexpr double GetDistToPlane() const { return dist_to_plane_; }
 };
 
