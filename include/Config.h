4302da9055982a94293b1cb77b5df963a92465ac
bogdan-lab <lakatosh@phystech.edu>
2022-04-23 15:02:49 +0300

    Add layout for Config class
    Currently realization is not added. For it I need to add parser library.
    Next commit will be about it.

---
+++ include/Config.h
@@ -0,0 +1,56 @@
+﻿#ifndef PATH_TRACER_CONFIG_H
+#define PATH_TRACER_CONFIG_H
+
+#include <fstream>
+#include <optional>
+#include <string_view>
+#include <vector>
+
+#include "Color.h"
+#include "Objects.h"
+
+struct CameraSettings {
+  // screen_plane
+  double distance_to_screen_;
+};
+
+struct GeneralSettings {
+  size_t sample_per_pixel_ = 0;
+  size_t max_bounce_number_ = 0;
+  std::string out_file_name_;
+};
+
+class Config {
+ public:
+  Config(std::string_view file_name);
+
+  const std::optional<CameraSettings>& GetCameraSettings() const {
+    return camera_settings_;
+  }
+  /**
+   * @brief Returns observer pointers, which will become dangling if the
+   * corresponding config instance will be destroyed.
+   */
+  std::vector<const Object*> GetObjectSettings() const {
+    std::vector<const Object*> result;
+    result.reserve(objects_.size());
+    for (const auto& el : objects_) {
+      result.push_back(el.get());
+    }
+    return result;
+  }
+
+  const std::optional<GeneralSettings>& GetGeneralSettings() const {
+    return general_settings_;
+  }
+
+ private:
+  static std::optional<GeneralSettings> ParseGeneralSettings(
+      std::ifstream& input);
+
+  std::optional<CameraSettings> camera_settings_;
+  std::optional<GeneralSettings> general_settings_;
+  std::vector<std::unique_ptr<Object>> objects_;
+};
+
+#endif  // PATH_TRACER_CONFIG_H
---
+++ src/config.cpp
@@ -0,0 +1,19 @@
+﻿#include "Config.h"
+
+#include <fstream>
+#include <optional>
+#include <string_view>
+
+Config::Config(std::string_view file_name) {
+  std::ifstream input(file_name);
+  if (!input.is_open()) {
+    std::cout << "Cannot open file " << file_name << '\n';
+    return;
+  }
+  general_settings_ = ParseGeneralSettings(input);
+}
+
+std::optional<GeneralSettings> Config::ParseGeneralSettings(
+    std::ifstream& input) {
+  return std::nullopt;
+}
